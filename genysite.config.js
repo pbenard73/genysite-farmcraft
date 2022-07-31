65105b275a9b35c2913e0d659beeb453c77f0375
BENARD Patrick <pbenard73@gmail.com>
2022-07-31 20:53:07 +0200

    git homepage

--- genysite.config.js
+++ genysite.config.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 
 module.exports = {
-  homepage: "file:///home/patrick/Workspace/themes/genysite-farmcraft/docs/",
+ // homepage: "file:///home/patrick/Workspace/themes/genysite-farmcraft/docs/",
+  homepage: "https://pbenard73.github.io/genysite-farmcraft/",
   react:true,
   index:'index',
 }
